68ca2f7bc4aacc0ccbba9346addafa7eb8365bec
tomonori-suzuki-biz <81912502+tomonori-suzuki-biz@users.noreply.github.com>
2021-08-21 15:10:23 +0900

    add index again

--- sql/0_Schema.sql
+++ sql/0_Schema.sql
@@ -36,4 +36,4 @@ CREATE TABLE `isu_association_config` (
   `url` VARCHAR(255) NOT NULL UNIQUE
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
 
--- ALTER TABLE isu_condition ADD INDEX index_on_uuid_timestamp(jia_isu_uuid, timestamp);
+ALTER TABLE isu_condition ADD INDEX index_on_uuid_timestamp(jia_isu_uuid, timestamp);
